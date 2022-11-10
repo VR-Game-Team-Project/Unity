f65ac3950bf24db0a87a4e9c56e2aa6bbe2e8489
Dong Jin <ilovethisid@naver.com>
2022-11-10 16:26:10 +0900

    Add zombie asset, make zombie go up

---
+++ Assets/zombie/GhoulController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhoulController : MonoBehaviour
+{
+    public float speed;
+    Animator animator;
+    private GameObject player;
+    private float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 dir = player.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(dir);
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, player.transform.position);
+        if (dist > 3.5f)
+        {
+            transform.position += transform.forward * speed;
+            //transform.position += new Vector3(transform.forward.x * speed, 0, transform.forward.z * speed);
+        }
+        else
+        {
+            animator.SetTrigger("nearPlayer");
+        }
+    }
+}
