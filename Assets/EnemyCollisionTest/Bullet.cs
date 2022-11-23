2c54a6e599879ee97ea084c400c8c25d34bec9c2
quadbeats <33650185+quadbeats@users.noreply.github.com>
2022-11-23 19:51:58 +0900

    collisiontest_scripts

---
+++ Assets/EnemyCollisionTest/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float bulletspeed;
+    void Start()
+    {
+        Destroy(gameObject, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * bulletspeed * Time.deltaTime;
+       
+    }
+}
+
+
